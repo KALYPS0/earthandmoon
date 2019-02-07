9c0bcd377fb4714734fca4d529e0d885027b5776
Calico Randall <calicorrandall@gmail.com>
2019-02-07 10:51:05 -0800

    changed translate size plus size and position of earth

--- moon.js
+++ moon.js
@@ -18,11 +18,11 @@ function draw(){
 	if (p<=4){
 		p=5;
 	}
-	translate(400,400);
+	translate(350,400);
 	myStars(random(width),random(height),random(3),random(10),p);
 	myStars(random(width),random(height),random(3),random(10),p);
 
-	myEarth(350,400,250);
+	myEarth(0,0,200);
 
 	let x = position + sin(angle) * radius;
 	let y = position + cos(angle) * radius/2;
